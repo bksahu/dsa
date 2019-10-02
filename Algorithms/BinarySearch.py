57f92aa8cbd3a81420bcbd1bb29b2208fa396a6e
bksahu <bablusahoo16@gmail.com>
2019-10-02 19:45:24 +0530

    Refractored BinarySearch

--- Algorithms/BinarySearch.py
+++ Algorithms/BinarySearch.py
@@ -28,5 +28,5 @@ def binary_search(input_array, value, start=0, end=None):
 test_list = [1,3,9,11,15,19,29]
 test_val1 = 25
 test_val2 = 15
-print binary_search(test_list, test_val1)
-print binary_search(test_list, test_val2)
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
