947d9630a396156102f04ddac8b5a8b009a74eb0
bksahu <bablusahoo16@gmail.com>
2020-12-26 14:20:55 +0530

    binary search code added along with some structural changes

---
+++ Algorithms/topological_sort.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('../')
+
+import data_structures
+
+# g = Graph({0: [1, 2], 1: [2], 2: [0, 3], 3: [3]})
+# g.DFS(2)
---
+++ patterns/binary_search/search_in_a_rotated_array_with_dupliactes.py
@@ -0,0 +1,90 @@
+"""
+Given an array of numbers which is sorted in ascending order and also rotated by some arbitrary number, 
+find if a given ‘key’ is present in it.
+
+Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, return -1. 
+You can assume that the given array does have duplicates.
+
+Input: [10, 15, 1, 3, 8], key = 15
+Output: 1
+Explanation: '15' is present in the array at index '1'.
+
+Input: [4, 5, 7, 9, 10, -1, 2], key = 10
+Output: 4
+Explanation: '10' is present in the array at index '4'.
+
+Input: [3, 7, 3, 3, 3], key = 7
+Output: 1
+Explanation: '7' is present in the array at index '1'.
+"""
+
+# def search_rotated_array(nums, key):
+#     left, right = 0, len(nums)-1
+
+#     if nums[left] < nums[right]:
+#         return binary_search(nums, left, right, key)
+#     else:
+#         while left <= right:
+#             mid = left + (right - left)//2
+#             if nums[mid] > nums[mid+1]: # mid is pivot
+#                 break
+
+#             if nums[mid] < nums[mid-1]: # mid - 1 is pivot
+#                 mid = mid - 1
+#                 break
+
+#             if nums[mid] < nums[left]:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+        
+#         if nums[mid] == key:
+#             return mid
+#         elif nums[mid] > key:
+#             return binary_search(nums, mid+1, right, key)
+#         else:
+#             return binary_search(nums, left, mid-1, key)
+
+# def binary_search(nums, left, right, key):
+
+#     while left <= right:
+#         mid = left + (right - left)//2
+#         if nums[mid] == key:
+#             return mid
+#         elif nums[mid] < key:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+
+#     return -1
+
+def search_rotated_array(nums, key):
+    left, right = 0, len(nums)-1
+
+    while left <= right:
+        mid = left + (right - left)//2
+
+        if nums[mid] == key:
+            return mid
+
+        if nums[left] == nums[mid] == nums[right]:
+            left += 1
+            right -= 1
+        elif nums[left] <= nums[mid]: # first half is sorted
+            if nums[left] <= key < nums[mid]:
+                right = mid - 1
+            else: # key is in the second half
+                left = mid + 1
+        else: # right half is sorted
+            if nums[mid] < key <= nums[right]:
+                left = mid + 1
+            else: # key is in the first half
+                right = mid - 1
+        
+    return -1
+
+if __name__ == "__main__":
+    print(search_rotated_array([10, 15, 1, 3, 8], 15))
+    print(search_rotated_array([4, 5, 7, 9, 10, -1, 2], 10))
+    print(search_rotated_array([3, 7, 3, 3, 3], 7))
+
