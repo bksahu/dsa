3eeee6638238749b6b84632bef169ce66cb2d76d
bksahu <bablusahoo16@gmail.com>
2021-03-01 20:34:58 +0530

    quicksort both left and right as pivot added

--- dsa/algorithms/quick_sort.py
+++ dsa/algorithms/quick_sort.py
@@ -1,9 +1,9 @@
 """
 This following code implements quick sort algorithm
 """
-import random
 
-def quickSort(arr):
+def quickSortLeft(arr):
+    # leftmost element is the pivot
     if len(arr) < 2:
         return arr
 
@@ -19,10 +19,33 @@ def quickSort(arr):
     # we now put pivot in it's right position
     arr[smallerThanPivotIdx], arr[0] = arr[0], arr[smallerThanPivotIdx]
 
-    return quickSort(arr[:smallerThanPivotIdx]) + [arr[smallerThanPivotIdx]] + quickSort(arr[smallerThanPivotIdx+1:])
-    
+    return quickSortLeft(arr[:smallerThanPivotIdx]) + \
+            [arr[smallerThanPivotIdx]] + \
+            quickSortLeft(arr[smallerThanPivotIdx+1:])
+
+def quickSortRight(arr, left, right):
+    # rightmost element is the pivot
+    if left < right:
+        pivotIdx = partion(arr, left, right)
+        quickSortRight(arr, left, pivotIdx-1)
+        quickSortRight(arr, pivotIdx+1, right)
+
+def partion(arr, left, right):
+    if left < right:
+        smallerThanPivotIdx = left - 1
+        for i in range(left, right):
+            if arr[i] <= arr[right]:
+                smallerThanPivotIdx += 1
+                arr[i], arr[smallerThanPivotIdx] = arr[smallerThanPivotIdx], arr[i]
+
+        arr[right], arr[smallerThanPivotIdx+1] = arr[smallerThanPivotIdx+1], arr[right]
+        return smallerThanPivotIdx + 1
 
 if __name__ == "__main__":
     array = [10, 5, 8, 9, 3, 6, 15, 12, 16]
     print("Before Sort: ", array)
-    print("After Sort: ", quickSort(array))
+    print("After Sort (Left as pivot): ", quickSortLeft(array.copy()))
+
+    print("Before Sort: ", array)
+    quickSortRight(array, 0, len(array)-1)
+    print("After Sort (Right as pivot): ", array)
